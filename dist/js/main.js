e8ae4f34c63f943d12bba9782343da6d17576ebd
Eugen <yanson.yauhen@gmail.com>
2021-07-13 21:01:42 +0300

    modal window

--- dist/js/main.js
+++ dist/js/main.js
@@ -1792,7 +1792,38 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  setClock('.timer', deadline);
+  setClock('.timer', deadline); // Modal
+
+  var modalTrigger = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelector('[data-close]');
+  modalTrigger.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      modal.classList.add('show');
+      modal.classList.remove('hide'); // Либо вариант с toggle - но тогда назначить класс в верстке
+
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show'); // Либо вариант с toggle - но тогда назначить класс в верстке
+
+    document.body.style.overflow = '';
+  }
+
+  modalCloseBtn.addEventListener('click', closeModal);
+  modal.addEventListener('click', function (e) {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+  document.addEventListener('keydown', function (e) {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
 });
 
 /***/ })
