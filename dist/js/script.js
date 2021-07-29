6eac563d49ffcccd3a355d4cf0b5d37a34e2ad56
Eugen <yanson.yauhen@gmail.com>
2021-07-29 22:47:27 +0300

    webpack

--- dist/js/script.js
+++ dist/js/script.js
@@ -5857,7 +5857,7 @@ window.addEventListener('DOMContentLoaded', function () {
   }, 50000);
   Object(_modules_tabs__WEBPACK_IMPORTED_MODULE_1__["default"])('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
   Object(_modules_modal__WEBPACK_IMPORTED_MODULE_2__["default"])('[data-modal]', '.modal', modalTimerId);
-  Object(_modules_timer__WEBPACK_IMPORTED_MODULE_3__["default"])('.timer', '2020-06-11');
+  Object(_modules_timer__WEBPACK_IMPORTED_MODULE_3__["default"])('.timer', '2021-08-08');
   Object(_modules_cards__WEBPACK_IMPORTED_MODULE_4__["default"])();
   Object(_modules_calc__WEBPACK_IMPORTED_MODULE_5__["default"])();
   Object(_modules_forms__WEBPACK_IMPORTED_MODULE_6__["default"])('form', modalTimerId);
